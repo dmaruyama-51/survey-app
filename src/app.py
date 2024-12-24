fd1169dd4e35efd9de9ba809c5d7077dacb32986
dmaruyama-51 <0501awasuram@gmail.com>
2024-12-25 00:10:52 +0900

    csvをアップロードしてプレビューするUIを追加

--- src/app.py
+++ src/app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Hello Streamlit!")
-st.write("Welcome to my Streamlit app!") 
+st.title("Survey Cleaning App")
+st.write("This app is used to clean survey data") 
+
+
+st.markdown("## Step1. upload the survey data")
+uploaded_file = st.file_uploader("Please upload the survey data", type=["csv"])
+
+if uploaded_file is not None:
+    st.write("Here is the uploaded data")
+    df = pd.read_csv(uploaded_file)
+else:
+    st.write("No file uploaded. Using sample data.")
+    df = pd.read_csv("src/data/sample.csv")
+
+st.write(df)
