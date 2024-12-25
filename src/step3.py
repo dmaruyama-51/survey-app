061770343eb7f19b5fc62d08974c6e483a7882e7
dmaruyama-51 <0501awasuram@gmail.com>
2024-12-25 14:37:17 +0900

    remove_out_of_range_values処理を追加

--- src/step3.py
+++ src/step3.py
@@ -20,7 +20,7 @@ def process_data_cleaning_and_export(
     req2 = st.checkbox("Remove rows with missing values")
     req3 = st.checkbox("Remove rows with maximum or minimum value exceeds the default")
 
-    remove_rows = remove_invalid_responses(df_to_process, req1, req2, req3)
+    remove_rows = remove_invalid_responses(df_to_process, likert_scale_case, req1, req2, req3)
     all_df = pd.concat([df_not_to_process, df_to_process], axis=1)
     cleaned_df = all_df.drop(index=remove_rows)
     removed_df = all_df.loc[remove_rows]
@@ -42,12 +42,13 @@ def process_data_cleaning_and_export(
 
 # pragma: no cover
 def remove_invalid_responses(
-    df: pd.DataFrame, req1: bool, req2: bool, req3: bool
+    df: pd.DataFrame, likert_scale_case: int, req1: bool, req2: bool, req3: bool
 ) -> List[int]:
     """
     指定された要件に基づいて無効な回答の行番号を取得
     Args:
         df (pd.DataFrame): 入力データフレーム
+        likert_scale_case (int): リッカート尺度のポイント数
         req1 (bool): ストレートライン回答を削除するかどうか
         req2 (bool): 欠損値を含む行を削除するかどうか
         req3 (bool): 規定値を超える行を削除するかどうか
@@ -62,6 +63,9 @@ def remove_invalid_responses(
         if req2:
             req2_remove_rows = remove_missing_values(df)
             remove_rows.extend(req2_remove_rows)
+        if req3:
+            req3_remove_rows = remove_out_of_range_values(df, likert_scale_case)
+            remove_rows.extend(req3_remove_rows)
 
         return remove_rows
     except Exception as e:
@@ -97,4 +101,22 @@ def remove_missing_values(df: pd.DataFrame) -> List[int]:
     if remove_rows:  # リストが空でない場合のみ表示
         st.write("以下の行番号に欠損値が見つかりました:")
         st.write(remove_rows)
+    return remove_rows
+
+def remove_out_of_range_values(df: pd.DataFrame, likert_scale_case: int) -> List[int]:
+    """
+    規定値を超える行番号を取得
+    Args:
+        df (pd.DataFrame): 入力データフレーム
+        likert_scale_case (int): リッカート尺度のポイント数
+    Returns:
+        List[int]: 規定値を超える行番号のリスト
+    """
+    remove_rows_over = df[df.max(axis=1) > likert_scale_case].index.tolist()
+    remove_rows_under = df[df.min(axis=1) < 1].index.tolist()
+    remove_rows = remove_rows_over + remove_rows_under
+    if remove_rows:  # リストが空でない場合のみ表示
+        st.write("以下の行番号に規定値の範囲外の値が見つかりました:")
+        st.write(remove_rows)
+
     return remove_rows
